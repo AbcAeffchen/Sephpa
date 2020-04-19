75a8ed3188ba17a990e0752857fe97bc1522c9db
Alex Schickedanz <alex@ae.cs.uni-frankfurt.de>
2020-04-19 02:29:15 +0200

    Update autoloader.

--- src/autoloader.php
+++ src/autoloader.php
@@ -3,7 +3,7 @@
  * Sephpa
  *
  * @license   GNU LGPL v3.0 - For details have a look at the LICENSE file
- * @copyright ©2018 Alexander Schickedanz
+ * @copyright ©2020 Alexander Schickedanz
  * @link      https://github.com/AbcAeffchen/Sephpa
  *
  * @author  Alexander Schickedanz <abcaeffchen@gmail.com>
@@ -13,13 +13,14 @@ namespace AbcAeffchen\Sephpa;
 
 function sephpaAutoloader($class)
 {
-    $class = preg_replace('#AbcAeffchen\\\Sephpa\\\(PaymentCollections\\\)?([^\.]+)#', '$2', $class);
+    $class = preg_replace('#AbcAeffchen\\\Sephpa\\\(PaymentCollections\\\)?([^.]+)#', '$2', $class);
 
     switch($class)
     {
         case 'Sephpa':
         case 'SephpaCreditTransfer':
         case 'SephpaDirectDebit':
+        case 'SephpaMultiFile':
             $file = __DIR__ . DIRECTORY_SEPARATOR  . $class . '.php';
             break;
         default:
